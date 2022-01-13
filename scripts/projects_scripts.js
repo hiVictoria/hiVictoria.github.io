1bd9bffa07c5ce3ce07bf4dd1d75b105e102dbda
Victoria Zhang <victoriazhanggg@gmail.com>
2022-01-13 16:26:28 -0600

    small refinements

--- scripts/projects_scripts.js
+++ scripts/projects_scripts.js
@@ -5,12 +5,30 @@ function displayOverlay(project_name){
 
     var title = project_name + "_title";
     document.getElementById(title).style.display ="block";
+}
+
+
+function displayOverlayCS(project_name){
+    var styleElem = document.head.appendChild(document.createElement("style"));
+    styleElem.innerHTML = "#" + project_name + ":after {content:no-open-quote;position: absolute;z-index: 3;height: 100%;width: 100%;background: #000;opacity: 0.7;}";
+
+    var title = project_name + "_title";
+    document.getElementById(title).style.display ="block";
 
     var cs = project_name + "_cs";
     document.getElementById(cs).style.display ="block";
 }
 
 function overlayOff(project_name){
+    var styleEl = document.head.appendChild(document.createElement("style"));
+    styleEl.innerHTML = "#" + project_name + ":after {opacity: 0;}";
+
+    var title = project_name + "_title";
+    document.getElementById(title).style.display ="none";
+
+}
+
+function overlayOffCS(project_name){
     var styleEl = document.head.appendChild(document.createElement("style"));
     styleEl.innerHTML = "#" + project_name + ":after {opacity: 0;}";
 
@@ -38,14 +56,14 @@ document.getElementById("womeninIS").addEventListener("mouseout", function(){ove
 document.getElementById("PLC").addEventListener("mouseover", function(){displayOverlay("PLC")});
 document.getElementById("PLC").addEventListener("mouseout", function(){overlayOff("PLC")});
 
-document.getElementById("TMC").addEventListener("mouseover", function(){displayOverlay("TMC")});
-document.getElementById("TMC").addEventListener("mouseout", function(){overlayOff("TMC")});
+document.getElementById("TMC").addEventListener("mouseover", function(){displayOverlayCS("TMC")});
+document.getElementById("TMC").addEventListener("mouseout", function(){overlayOffCS("TMC")});
 
-document.getElementById("graphic_art").addEventListener("mouseover", function(){displayOverlay("graphic_art")});
-document.getElementById("graphic_art").addEventListener("mouseout", function(){overlayOff("graphic_art")});
+document.getElementById("graphic_art").addEventListener("mouseover", function(){displayOverlayCS("graphic_art")});
+document.getElementById("graphic_art").addEventListener("mouseout", function(){overlayOffCS("graphic_art")});
 
-document.getElementById("graphic_square").addEventListener("mouseover", function(){displayOverlay("graphic_square")});
-document.getElementById("graphic_square").addEventListener("mouseout", function(){overlayOff("graphic_square")});
+document.getElementById("graphic_square").addEventListener("mouseover", function(){displayOverlayCS("graphic_square")});
+document.getElementById("graphic_square").addEventListener("mouseout", function(){overlayOffCS("graphic_square")});
 
 document.getElementById("nav_home").addEventListener("mouseover", lightenText);
 document.getElementById("nav_home").addEventListener("mouseout", darkenText);
